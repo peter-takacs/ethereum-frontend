558a2ceab802993877b02090ffcd7ed11e673eec
Peter Takacs <hhike@outlook.com>
2018-08-18 15:36:31 +0200

    Added layout with material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,72 @@ import AccountDisplay from './containers/account';
 
 import { Link, Route } from 'react-router-dom';
 
-const App = () => (
-      <div>
-        <nav>
-          <Link to="members">Members</Link>
-          <Link to="check">Check certificate</Link>
-          <Link to="assign">Assign certificate</Link>
-        </nav>
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, Typography, Drawer, withStyles, createStyles, Theme, WithStyles, List, ListItem, ListItemText } from '@material-ui/core';
+
+const drawerWidth = 240;
+const styles = (theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawerPaper: {
+    position: 'relative', 
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    minWidth: 0, // So the Typography noWrap works
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+const links = [
+  {path: 'members', title: 'Members'},
+  {path: 'check', title: 'Check certificate'},
+  {path: 'assign', title: 'Assign certificate'},
+]
+
+const App = (props: WithStyles<typeof styles>) => {
+  const classes = props.classes;
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="title" color="inherit" noWrap>
+            Ethereum frontend
+            </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" classes={{
+        paper: classes.drawerPaper
+      }}>
+        <div className={classes.toolbar} />
+        <List>
+          {links.map(link => (
+            <ListItem>
+              <Link to={link.path} >{link.title} </Link>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <AccountDisplay />
-        <Route path="/members" component={NetworkMembers}/>
+        <Route path="/members" component={NetworkMembers} />
         <Route path="/check" component={CertificateChecker} />
         <Route path="/assign" component={CertificateAdder} />
-      </div>
-    );
+      </main>
+    </div>
+  );
+  }
 
-export default App
+export default withStyles(styles)(App);
