6804e0a7939f911be00692dc72b4eed804642e04
Peter Takacs <hhike@outlook.com>
2018-08-25 13:42:45 +0200

    Update npm dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import App from './App'
+import * as renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('App component', () => {
+
+  it('Renders correctly', () => {
+    const tree = renderer
+      .create(<App/>)
+      .toJSON();
+    
+  })
+
 })
