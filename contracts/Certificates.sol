6af0afb7d2b648d917cc9448b0d5b407c2b1d42b
Peter Takacs <hhike@outlook.com>
2018-08-25 18:27:08 +0200

    Store issuer and isRevoked in contracts

--- contracts/Certificates.sol
+++ contracts/Certificates.sol
@@ -3,7 +3,13 @@ import "./EducatorNetwork.sol";
 
 contract Certificates {
 
-    mapping(address => uint256[]) registry;
+    struct Assertion {
+        uint256 hashedAssertion;
+        address issuer;
+        bool isRevoked;
+    }
+
+    mapping(address => Assertion[]) registry;
     EducatorNetwork educatorNetwork;
     
     constructor(address educatorNetworkAddress) public {
@@ -11,7 +17,17 @@ contract Certificates {
     }
     
     function getCertificates(address candidate) public view returns(uint256[]) {
-        return registry[candidate];
+        if (registry[candidate].length == 0) {
+            return new uint256[](0);
+        }
+        Assertion[] memory candidateAssertions = registry[candidate];
+        uint256[] memory result = new uint256[](candidateAssertions.length);
+        for (uint i = 0; i < result.length; i++) {
+            if (!candidateAssertions[i].isRevoked) {
+                result[i] = candidateAssertions[i].hashedAssertion;
+            }
+        }
+        return result;
     }
 
     function contains(uint256[] list, uint256 entry) private pure returns (bool) {
@@ -23,30 +39,16 @@ contract Certificates {
         return false;
     }
 
-    function remove(uint256[] list, uint256 element) private view returns (uint256[]) {
-        if (contains(list, element)) {
-            uint256[] memory result = new uint256[](list.length);
-            uint originalIndex = 0;
-            uint newIndex = 0;
-            while (originalIndex < list.length) {
-                if (list[originalIndex] != element) {
-                    result[newIndex] = list[originalIndex];
-                    newIndex++;
-                }
-                originalIndex++;
-            }
-            list[list.length - 1] = 0;
-            for (uint i = 0; i < list.length; i++) {
-                list[i] = result[i];
-            }
-        }
-        return list;
-    }
-    
     function revoke(address holder, uint256 certificate) public returns (bool) {
         address sender = msg.sender;
+        Assertion[] memory candidateAssertions = registry[holder];
         if (educatorNetwork.isMember(sender)) {
-            registry[holder] = remove(registry[holder], certificate);
+            for (uint i = 0; i < candidateAssertions.length; i++) {
+                if (candidateAssertions[i].hashedAssertion == certificate) {
+                    candidateAssertions[i].isRevoked = true;
+                    return true;
+                }
+            }
         }
         revert();
         return false;
@@ -55,7 +57,11 @@ contract Certificates {
     function assign(address recipient, uint256 certificate) public returns (bool) {
         address sender = msg.sender;
         if (educatorNetwork.isMember(sender)) {
-            registry[recipient].push(certificate);
+            Assertion memory assertion;
+            assertion.hashedAssertion = certificate;
+            assertion.isRevoked = true;
+            assertion.issuer = sender;
+            registry[recipient].push(assertion);
             return true;
         }
         revert();
