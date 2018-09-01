4024336cddc9538e735988f767aebe55a42fd98f
Peter Takacs <hhike@outlook.com>
2018-09-01 09:42:07 +0200

    Add utility functions for votes

--- contracts/EducatorNetwork.sol
+++ contracts/EducatorNetwork.sol
@@ -3,18 +3,25 @@ pragma solidity 0.4.24;
 contract EducatorNetwork {
     
     event logUint(uint256);
-    
+
+    enum VoteStatus {
+        Pending,
+        Accept,
+        Reject
+    }
+
     address[] members;
-    mapping(address => mapping(address => bool)) votesInProgress;
+    address[] candidates;
+    mapping(address => mapping(address => VoteStatus)) votesInProgress;
     
-    function castVote(address sender, address newMember, bool vote) private {
+    function castVote(address sender, address newMember, VoteStatus vote) private {
         votesInProgress[newMember][sender] = vote;
     }
     
     function checkIfPastThreshold(address newMember) private view returns(bool) {
         uint positiveVotes = 0;
         for (uint i = 0; i < members.length; i++) {
-            if (votesInProgress[newMember][members[i]]) {
+            if (votesInProgress[newMember][members[i]] == VoteStatus.Accept) {
                 positiveVotes++;
             }
         }
@@ -28,18 +35,41 @@ contract EducatorNetwork {
         members = new address[](1);
         members[0] = msg.sender;
     }
-    
-    function isMember(address member) public view returns (bool){
-        for (uint i = 0; i < members.length; i++) {
-            if (members[i] == member) {
+
+    function contains(address[] haystack, address needle) private pure returns (bool) {
+        for (uint i = 0; i < haystack.length; i++) {
+            if (haystack[i] == needle) {
                 return true;
             }
         }
+
         return false;
+    }
+    
+    function isMember(address member) public view returns (bool){
+        return contains(members, member);
     }
     
     function getMembers() public view returns(address[]) {
         return members;
+    }
+
+    function getCandidates() public view returns (address[]) {
+        return candidates;
+    }
+
+    function getVotesForCandidate(address candidate) public view returns(address[], VoteStatus[]) {
+        if (!contains(candidates, candidate)) {
+            return (new address[](0), new VoteStatus[](0));
+        }
+        address[] memory resultAddresses = new address[](members.length);
+        VoteStatus[] memory resultVoteStatuses = new VoteStatus[](members.length);
+        mapping(address => VoteStatus) castVotes = votesInProgress[candidate];
+        for (uint i = 0; i < members.length; i++) {
+            resultAddresses[i] = members[i];
+            resultVoteStatuses[i] = castVotes[members[i]];
+        }
+        return (resultAddresses, resultVoteStatuses);
     }
     
     function requestAddition(address newMember) public {
@@ -49,11 +79,23 @@ contract EducatorNetwork {
         if (!isMember(msg.sender)) {
             revert();
         }
-        castVote(msg.sender, newMember, true);
+
+        if (!contains(candidates, newMember)) {
+            address[] memory updatedCandidates = new address[](candidates.length + 1);
+            for (uint i = 0; i < members.length; i++) {
+                updatedCandidates[i] = members[i];
+            }
+            updatedCandidates[members.length] = newMember;
+            candidates = updatedCandidates;
+        }
+
+        castVote(msg.sender, newMember, VoteStatus.Accept);
+        
+        
         if (checkIfPastThreshold(newMember)) {
             address[] memory updatedMembers = new address[](members.length + 1);
-            for (uint i = 0; i < members.length; i++) {
-                updatedMembers[i] = members[i];
+            for (uint j = 0; j < members.length; j++) {
+                updatedMembers[j] = members[j];
             }
             updatedMembers[members.length] = newMember;
             members = updatedMembers;
