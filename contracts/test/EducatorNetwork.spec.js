182f1ea44e9077edc0bf0bdb210f68a544a48114
Peter Takacs <hhike@outlook.com>
2018-09-01 11:33:34 +0200

    Add unit test for educatornetwork contract

--- contracts/test/EducatorNetwork.spec.js
+++ contracts/test/EducatorNetwork.spec.js
@@ -1,6 +1,12 @@
 const EducatorNetwork = artifacts.require('EducatorNetwork');
+const catchRevert = require("./exceptions.js").catchRevert;
 
 contract('Test initial state', async (accounts) => {
+
+    // NOTE: contract state is NOT reset within the same contract()
+    // block. `it` sections are executed in the order they appear
+    // in the source. There are dependencies between the `it` sections,
+    // keep that in mind when reordering or parallel testing.
 
     let initialAccount = accounts[0];
 
@@ -11,13 +17,57 @@ contract('Test initial state', async (accounts) => {
     });
 
     it('Should add a new member if requested', async() => {
+        let additionalAccount = accounts[1];
+        
         let instance = await EducatorNetwork.deployed();
 
-        await instance.requestAddition.call(accounts[1], {from: initialAccount, gas: 500000});
+        await instance.requestAddition(accounts[1], 
+            {from: initialAccount, gas: 500000}
+        );
 
-        let members = await instance.getMembers();
-        console.log(members);
-        assert.include(members, accounts[0]);
-        assert.include(members, accounts[1]);
+        let members = await instance.getMembers.call();
+        let candidates = await instance.getCandidates.call();
+        assert.include(members, initialAccount);
+        assert.include(members, additionalAccount);
+        assert.include(candidates, additionalAccount);
+    });
+
+    it('Should not add a new member without majority', async() => {
+        let secondAccount = accounts[2];
+
+        let instance = await EducatorNetwork.deployed();
+        await instance.requestAddition(secondAccount, {from: initialAccount, gas: 500000});
+
+        let members = await instance.getMembers.call();
+        let candidates = await instance.getCandidates.call();
+        assert.notInclude(members, secondAccount);
+        assert.include(candidates, secondAccount);
+    });
+
+    it('Should add the new member with majority', async () => {
+        let secondAccount = accounts[2];
+
+        let instance = await EducatorNetwork.deployed();
+        await instance.requestAddition(secondAccount, {from: accounts[1], gas: 500000});
+
+        let members = await instance.getMembers.call();
+        let candidates = await instance.getCandidates.call();
+        assert.include(members, secondAccount);
+        assert.include(candidates, secondAccount);
+    });
+
+    it('Should not add a candidate if the submitter is not a member', async () => {
+        let dummyAccount = accounts[3];
+
+        let instance = await EducatorNetwork.deployed();
+        await catchRevert(
+            instance.requestAddition(accounts[4], {from: dummyAccount, gas: 500000})
+        );
+
+        let members = await instance.getMembers.call();
+        let candidates = await instance.getCandidates.call();
+
+        assert.notInclude(members, accounts[4]);
+        assert.notInclude(candidates, accounts[4]);
     })
 })
