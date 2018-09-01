3efff39516fddaa0de67b5e281e10243e058cf72
Peter Takacs <hhike@outlook.com>
2018-09-01 15:30:16 +0200

    Add tests for candidate-status

--- src/components/network/candidate-status.spec.tsx
+++ src/components/network/candidate-status.spec.tsx
@@ -1,14 +1,16 @@
+
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import { dummyAddress } from '../../utils/test/address';
-import VoteStatus from './vote-status';
+import CandidateStatus from './candidate-status';
 import { VoteStatus as VoteStatusEnum } from '../../types/vote' ;
 import { Address } from '../../types/ethereum-address';
 
 test('Renders correctly with no data', () => {
     const component = renderer.create(
-            <VoteStatus
-                candidateStatuses={[]}
+            <CandidateStatus
+                candidate={null}
+                votes={new Map<Address, VoteStatusEnum>()} 
             />
         );
         const tree = component.toJSON();
@@ -19,13 +21,9 @@ test('Renders correctly with data', () => {
     const votes = new Map<Address, VoteStatusEnum>();
     votes.set(dummyAddress(1), VoteStatusEnum.Accept);
     const component = renderer.create(
-        <VoteStatus
-            candidateStatuses={[
-                {
-                    candidate: dummyAddress(0),
-                    votes
-                }
-            ]}
+        <CandidateStatus
+            candidate={dummyAddress(0)}
+            votes={votes}
         />
     );
     const tree = component.toJSON();
