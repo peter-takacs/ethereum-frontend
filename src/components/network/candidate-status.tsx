1b973cdf82e2e34cb5113b96386fa6ff0e7559ee
Peter Takacs <hhike@outlook.com>
2018-09-08 18:52:14 +0200

    Update vote status dipsplay

--- src/components/network/candidate-status.tsx
+++ src/components/network/candidate-status.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Address } from '../../types/ethereum-address';
 import { VoteStatus } from '../../types/vote';
-import { Grid, Typography, Button } from '@material-ui/core';
+import { Grid, Typography, Button, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import { AccountState } from '../../state/account';
 import VotingButtons from './voting-buttons';
 
@@ -22,31 +22,56 @@ export interface CandidateStatusDispatch {
 export type CandidateStatusProps = AccountState & CandidateStatusState & OwnProps;
 
 class CandidateStatus extends React.Component<CandidateStatusProps & CandidateStatusDispatch> {
+    
+    private translateStatus(voteStatus: VoteStatus) {
+        switch (voteStatus) {
+            case VoteStatus.Accept:
+                return (<Typography>Accepted</Typography>);
+            case VoteStatus.Reject:
+                return (<Typography>Rejected</Typography>);
+            case VoteStatus.Pending:
+                return (<Typography>Pending</Typography>);
+        }
+    }
+    
     public render(): JSX.Element {
         if (this.props.candidate != null)
         {
             const candidateToVote = this.props.candidate;
             return (
-                <Grid container>
-                    {Array.from(this.props.votes).map(([key, status]) => (
-                        <Grid key={"v-" + key.toString()} container spacing={24}>
-                            <Grid item>
-                                {key.toString()}
-                            </Grid>
-                            <Grid item>
-                                {status}
-                            </Grid>
-                            {this.props.address && this.props.address.equals(key) 
-                                ? (
-                                    <VotingButtons 
-                                        candidate={candidateToVote}
-                                        onAccept={this.props.onAccept}
-                                        onReject={this.props.onReject}
-                                    />)
-                                : (<Grid item/>)}
-                        </Grid>
-                    ))}
-                </Grid>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Voter</TableCell>
+                            <TableCell>Status</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {Array.from(this.props.votes).map(([key, status]) => (
+                            <TableRow key={"v-" + key.toString()}>
+                                <TableCell>
+                                    {key.toString()}
+                                </TableCell>
+                                <TableCell>
+                                    {this.translateStatus(status)}
+                                </TableCell>
+                                <TableCell>
+                                    {this.props.address && 
+                                    this.props.address.equals(key) &&
+                                    status === VoteStatus.Pending   
+                                        ? (
+                                            <VotingButtons
+                                                candidate={candidateToVote}
+                                                onAccept={this.props.onAccept}
+                                                onReject={this.props.onReject}
+                                            />)
+                                        : (<Grid item />)}
+                                </TableCell>
+                        </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
             );
         }
         return (<div/>);
